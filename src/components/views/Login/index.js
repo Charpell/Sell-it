878264be8ec23b5500a7fa7f498fc714279025bb
Charpell <charpellumeh@gmail.com>
2018-08-04 21:26:41 +0100

    Adding Tabs

--- src/components/views/Login/index.js
+++ src/components/views/Login/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import LoadTabs from '../Tabs';
 
 class Login extends Component {
   render() {
     return (
-      <Text>Login</Text>
+      <View>
+        <Text>Login</Text>
+        <Button 
+          title="go to home"
+          onPress={() => { LoadTabs()} }
+        />
+      </View>
     );
   }
 }
