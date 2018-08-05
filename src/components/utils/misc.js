ddbecaa05057207636669a60397cba9367ddc9e5
Charpell <charpellumeh@gmail.com>
2018-08-05 13:41:22 +0100

    Storing the Tokens in AsynStorage

--- src/components/utils/misc.js
+++ src/components/utils/misc.js
@@ -1,5 +1,5 @@
 import { Dimensions,
-  Platform, } from 'react-native';
+  Platform, AsyncStorage } from 'react-native';
 
 export const getOrientation = (value) => {
   return Dimensions.get("window").height > value ?  "portrait" : "landscape" 
@@ -19,4 +19,30 @@ export const getPlatform = () => {
   } else {
       return "android"
   }
+}
+
+export const getTokens = (callbackFunction) => {
+  AsyncStorage.multiGet([
+      '@sellitApp@token',
+      '@sellitApp@refreshToken',
+      '@sellitApp@expireToken',
+      '@sellitApp@uid',
+  ]).then(value=>{
+      callbackFunction(value)
+  })
+}
+
+
+export const setTokens = (values, callbackFunction) => {
+  const dateNow = new Date();
+  const expiration = dateNow.getTime() + (3600 * 1000);
+
+  AsyncStorage.multiSet([
+      ['@sellitApp@token', values.token],
+      ['@sellitApp@refreshToken', values.refToken],
+      ['@sellitApp@expireToken', expiration.toString()],
+      ['@sellitApp@uid', values.uid],
+  ]).then( response => {
+      callbackFunction();
+  })
 }
--- src/components/views/Login/loginForm.js
+++ src/components/views/Login/loginForm.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 import Input from "../../utils/forms/input";
-import ValidationRules from '../../utils/forms/validationRules';
-import LoadTabs from '../Tabs';
+import ValidationRules from "../../utils/forms/validationRules";
+import LoadTabs from "../Tabs";
 
-import { connect } from 'react-redux';
-import { signUp, signIn } from '../../Store/actions/user_actions';
-import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
+import { signUp, signIn } from "../../Store/actions/user_actions";
+import { bindActionCreators } from "redux";
+import { setTokens } from '../../utils/misc';
 
 class LoginForm extends Component {
   state = {
-    type:'Login',
-    action:'Login',
-    actionMode:'Not a user, Register',
+    type: "Login",
+    action: "Login",
+    actionMode: "Not a user, Register",
     hasErrors: false,
     form: {
       email: {
@@ -53,44 +54,54 @@ class LoginForm extends Component {
     let formCopy = this.state.form;
     formCopy[name].value = value;
 
-    let rules = formCopy[name].rules
+    let rules = formCopy[name].rules;
     let valid = ValidationRules(value, rules, formCopy);
 
     formCopy[name].valid = valid;
-
 
     this.setState({
       form: formCopy
     });
   };
 
-  confirmPassword = () => (
-    this.state.type != 'Login' ?
-        <Input
-            placeholder="Confirm your password"
-            type={this.state.form.confirmPassword.type}
-            value={this.state.form.confirmPassword.value}
-            onChangeText={ value => this.updateInput("confirmPassword",value) }
-            secureTextEntry
-        />
-    :null
-  )
+  confirmPassword = () =>
+    this.state.type != "Login" ? (
+      <Input
+        placeholder="Confirm your password"
+        type={this.state.form.confirmPassword.type}
+        value={this.state.form.confirmPassword.value}
+        onChangeText={value => this.updateInput("confirmPassword", value)}
+        secureTextEntry
+      />
+    ) : null;
 
-  formHasErrors = () => (
-    this.state.hasErrors ? 
-        <View style={styles.errorContainer}>
-            <Text style={styles.errorLabel}>Opps, check your info</Text>
-        </View>
-    :null
-  )
+  formHasErrors = () =>
+    this.state.hasErrors ? (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorLabel}>Opps, check your info</Text>
+      </View>
+    ) : null;
 
   changeFormType = () => {
     const type = this.state.type;
     this.setState({
-        type: type === 'Login' ? 'Register' : 'Login',
-        action: type === 'Login' ? 'Register' : 'Login',
-        actionMode:  type === 'Login' ? 'Not registered, Login' : 'Not a user, Register',
-    })
+      type: type === "Login" ? "Register" : "Login",
+      action: type === "Login" ? "Register" : "Login",
+      actionMode:
+        type === "Login" ? "Not registered, Login" : "Not a user, Register"
+    });
+  };
+
+
+  manageAccess = () => {
+    if(!this.props.User.userData.uid){
+        this.setState({hasErrors:true})
+    } else {
+        setTokens(this.props.User.userData, () => {
+            this.setState({hasErrors:false});
+            LoadTabs();
+        })
+    }
   }
 
   submitUser = () => {
@@ -98,38 +109,36 @@ class LoginForm extends Component {
     let formToSubmit = {};
     const formCopy = this.state.form;
 
-    for(let key in formCopy){
-        if(this.state.type === 'Login'){
-            // Login
-            if(key !== 'confirmPassword'){
-                isFormValid = isFormValid && formCopy[key].valid;
-                formToSubmit[key] = formCopy[key].value
-            }
-        } else {
-            // Register
-            isFormValid = isFormValid && formCopy[key].valid;
-            formToSubmit[key] = formCopy[key].value;
+    for (let key in formCopy) {
+      if (this.state.type === "Login") {
+        // Login
+        if (key !== "confirmPassword") {
+          isFormValid = isFormValid && formCopy[key].valid;
+          formToSubmit[key] = formCopy[key].value;
         }
-    }
-
-    if(isFormValid){
-      if(this.state.type === "Login"){
-        this.props.signIn(formToSubmit).then(()=>{
-          console.log('User', this.props.User)
-        })
-        } else { 
-              this.props.signUp(formToSubmit).then(()=>{
-                  console.log('User', this.props.User)
-              })
+      } else {
+        // Register
+        isFormValid = isFormValid && formCopy[key].valid;
+        formToSubmit[key] = formCopy[key].value;
       }
-
-    } else {
-        this.setState({
-            hasErrors:true
-        })
     }
-  }
 
+    if (isFormValid) {
+      if (this.state.type === "Login") {
+        this.props.signIn(formToSubmit).then(() => {
+          this.manageAccess();
+        });
+      } else {
+        this.props.signUp(formToSubmit).then(() => {
+          this.manageAccess();
+        });
+      }
+    } else {
+      this.setState({
+        hasErrors: true
+      });
+    }
+  };
 
   render() {
     return (
@@ -153,7 +162,6 @@ class LoginForm extends Component {
 
         {this.confirmPassword()}
         {this.formHasErrors()}
-
 
         <View
           style={
@@ -187,7 +195,7 @@ class LoginForm extends Component {
           <Button
             title="I'll do it later"
             color="lightgrey"
-            onPress={()=> LoadTabs()}
+            onPress={() => LoadTabs()}
           />
         </View>
       </View>
@@ -200,32 +208,32 @@ const styles = StyleSheet.create({
     minHeight: 400
   },
   buttonStyleAndroid: {
-    marginBottom:10,
-    marginTop:10
+    marginBottom: 10,
+    marginTop: 10
   },
   buttonStyleIos: {
-      marginBottom:0
+    marginBottom: 0
   },
   errorContainer: {
-    marginBottom:20,
-    marginTop:10
+    marginBottom: 20,
+    marginTop: 10
   },
-  errorLabel:{
-    color:'red',
+  errorLabel: {
+    color: "red"
   }
 });
 
-
-function mapStateToProps(state){
-  return{
-      User: state.User
-  }
+function mapStateToProps(state) {
+  return {
+    User: state.User
+  };
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({signUp,signIn},dispatch)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ signUp, signIn }, dispatch);
 }
 
-
-export default connect(mapStateToProps,mapDispatchToProps)(LoginForm);
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
