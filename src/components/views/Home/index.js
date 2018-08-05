affde1dcb64a0afa67300b3c8c7aab72a0c612c2
Charpell <charpellumeh@gmail.com>
2018-08-05 22:33:30 +0100

    Finishing Horizontal Scroll

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,33 +1,42 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
-import { navigatorDrawer, navigatorDeepLink } from '../../utils/misc';
-import HorizontalScroll from './horizontal_scroll_icons';
+import { navigatorDrawer, navigatorDeepLink } from "../../utils/misc";
+import HorizontalScroll from "./horizontal_scroll_icons";
 
 class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      isLoading:true,
-      articles:[],
-      categories:['All','Sports','Music','Clothing','Electronics'],
-      categorySelected:"All"
-    }
+      isLoading: true,
+      articles: [],
+      categories: ["All", "Sports", "Music", "Clothing", "Electronics"],
+      categorySelected: "All"
+    };
 
-    this.props.navigator.setOnNavigatorEvent((event) => {
-      navigatorDeepLink(event, this)
-      navigatorDrawer(event, this)
-    })
-  } 
+    this.props.navigator.setOnNavigatorEvent(event => {
+      navigatorDeepLink(event, this);
+      navigatorDrawer(event, this);
+    });
+  }
+
+  updateCategoryHandler = value => {
+    this.setState({
+      isLoading: true,
+      categorySelected: value,
+      articles: []
+    });
+  };
 
   render() {
     return (
       <ScrollView>
         <View style={styles.container}>
-          <HorizontalScroll 
+          <HorizontalScroll
             categories={this.state.categories}
             categorySelected={this.state.categorySelected}
+            updateCategoryHandler={this.updateCategoryHandler}
           />
         </View>
       </ScrollView>
@@ -36,9 +45,7 @@ class Home extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  }
+  container: {}
 });
 
 export default Home;
